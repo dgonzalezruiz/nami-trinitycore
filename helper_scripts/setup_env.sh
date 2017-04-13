508eaea89da045b6ea5a02aca06c65763229c9e6
David Gonzalez <dgonzalez@bitnami.com>
2017-04-13 14:45:49 +0000

    Testing build behaviour

--- helper_scripts/setup_env.sh
+++ helper_scripts/setup_env.sh
@@ -1,41 +1,22 @@
 #!/bin/bash 
-
 set -e
-## SQL set release used
+
+## SQL set release used; will be downloaded and integrated in the released tarball
 export SQL_FILES=TDB_full_335.62_2016_10_17
 SERVER_SQL=https://github.com/TrinityCore/TrinityCore/releases/download/TDB335.62/$SQL_FILES.7z
 
+## Packages are installed now
+sudo apt-get update -qq
+sudo apt-get -qq install build-essential libtool make cmake cmake-data openssl p7zip   \
+                        libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev  \
+                        zlib1g-dev libbz2-dev libboost1.55-dev libboost-thread1.55-dev \
+                        libboost-filesystem1.55-dev libboost-system1.55-dev            \
+                        libboost-program-options1.55-dev libboost-iostreams1.55-dev    \
+                        libboost-regex1.55-dev
 
-## Retrieve all stuff required from the internet
-#git clone -b 3.3.5 --single-branch https://github.com/TrinityCore/TrinityCore 
-#git clone https://github.com/bitnami/blacksmith
+## The repository with the source to compile is cloned 
+git clone -b 3.3.5 --single-branch https://github.com/TrinityCore/TrinityCore 
+
+## The source SQL files are retrieved and extracted
 wget $SERVER_SQL
-
-## Move SQL set to compilation environment
 p7zip -d $SQL_FILES.7z
-#mv $SQL_FILES $workingDir/TrinityCore
-
-
-## Add stuff to path
-#export PATH=$workingDir/blacksmith/bin:$PATH
-
-## Build the docker image that we'll use later for compilation purposes
-#docker build compilation -t buildenvironment
-
-## Set up the compilation tool
-#cd $workingDir/blacksmith
-#npm install
-#npm run install-runtime
-#blacksmith=$workingDir/blacksmith/bin/blacksmith
-#$blacksmith --help
-#$blacksmith configure containerizedBuild.images[0].id buildenvironment
-#$blacksmith configure paths.output /tmp/trinitycore/blacksmith-output
-#$blacksmith configure paths.sandbox /tmp/trinitycore/blacksmith-sandbox
-#$blacksmith configure paths.recipes $workingDir/compilation
-#$blacksmith configure compilation.prefix /opt/trinitycore
-
-## Compress the source repo for compilation
-cd $workingDir
-#tar czf trinitycore.tar.gz TrinityCore
-
-
