54fcb66fa9532f0ccc07d68b427fe7c876ff2071
David Gonzalez <dgonzalez@bitnami.com>
2017-04-15 15:44:19 +0000

    Stick to clang 3.9

--- helper_scripts/setup_env.sh
+++ helper_scripts/setup_env.sh
@@ -5,14 +5,20 @@ set -e
 SQL_FILES=TDB_full_335.62_2016_10_17
 SERVER_SQL=https://github.com/TrinityCore/TrinityCore/releases/download/TDB335.62/$SQL_FILES.7z
 
+##Update mirrors for CLANG overriding
+wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+yes | sudo add-apt-repository 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main'
+
 ## Packages are installed now
 sudo apt-get update -qq
-sudo apt-get -qq install build-essential libtool make cmake cmake-data openssl p7zip   \
-                        libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev  \
-                        zlib1g-dev libbz2-dev libboost1.55-dev libboost-thread1.55-dev \
-                        libboost-filesystem1.55-dev libboost-system1.55-dev            \
-                        libboost-program-options1.55-dev libboost-iostreams1.55-dev    \
-                        libboost-regex1.55-dev
+sudo apt-get -qq install clang-3.9 build-essential libtool make cmake cmake-data openssl \
+                         libssl-dev libmysqlclient-dev libmysql++-dev libreadline6-dev   \
+                         zlib1g-dev libbz2-dev libboost1.55-dev libboost-thread1.55-dev  \
+                         libboost-filesystem1.55-dev libboost-system1.55-dev             \
+                         libboost-program-options1.55-dev libboost-iostreams1.55-dev
+
+## Set up proper compiler variables 
+export CC=clang-3.9 CXX=clang++-3.9
 
 ## The repository with the source to compile is cloned 
 git clone -b 3.3.5 --single-branch https://github.com/TrinityCore/TrinityCore 
