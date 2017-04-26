7f55cf34148b4429a3bd1cebca46070aeb4b20b5
David González <dgonzalez@bitnami.com>
2017-04-27 01:01:23 +0200

    [ci skip] Add retrieval of tarball to setup_env

--- helper_scripts/setup_env.sh
+++ helper_scripts/setup_env.sh
@@ -1,6 +1,18 @@
 #!/bin/bash 
 set -e
 
+## The released tarball with the compiled binaries is retrieved
+git fetch tags
+GIT_TAG="$(git tag | tail -n1)"
+URL="https://github.com/dgonzalezruiz/trinitycore-builds/releases/download/$GIT_TAG/trinitycore-$GIT_TAG.tar.gz"
+URL_STATUS="curl -o /dev/null --silent --head --write-out '%{http_code}\n' $URL"
+while [[ "$($URL_STATUS)" == *"4"* || "$($URL_STATUS)" == *"5"* ]] ; do 
+  sleep 1 
+  echo "Waiting for released binary to be available..." 
+done
+
+curl -L $URL > $TRAVIS_BUILD_DIR/trinitycore.tar.gz 
+
 ##Update mirrors for CLANG overriding
 wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 yes | sudo add-apt-repository 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-3.9 main'
@@ -16,8 +28,6 @@ sudo apt-get -qq install clang-3.9 build-essential libtool make cmake cmake-data
 ## Set up proper compiler variables 
 export CC=clang-3.9 CXX=clang++-3.9
 
-## The repository with the source to compile is cloned 
-git clone -b 3.3.5 --single-branch https://github.com/TrinityCore/TrinityCore 
 
 ## The source SQL files are retrieved and extracted
 wget $WORLD_SQL_URL
