3401bf6bb544f2b645022974c13178654f89bef6
David González <dgonzalez@bitnami.com>
2017-05-09 18:00:23 +0200

    Better debugging

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -22,9 +22,9 @@ while [[ "$($URL_STATUS)" == *"4"* || "$($URL_STATUS)" == *"5"* ]] ; do
   sleep 1 
   echo "Waiting for released binary to be available..." 
 done
-ls $TRAVIS_BUILD_DIR/trinitycore.tar.gz 
-ls $TRAVIS_BUILD_DIR/trinitycore
-ls $TRAVIS_BUILD_DIR/tests/rootfs
+
+ls /usr/lib/x86_64-linux-gnu/libmysqlclient_r.so
+ls /usr/include/mysql
 
 curl -L $URL > $TRAVIS_BUILD_DIR/trinitycore.tar.gz 
 
